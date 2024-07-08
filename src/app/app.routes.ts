493d36b7bfb1d24c6ee5fc273d87ee22638c6607
Carlos <017100872b@uandina.edu.pe>
2024-07-08 12:13:42 -0500

    prueba_Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { HotelComponent } from './pages/hotel/hotel.component';
+import { TrainComponent } from './pages/train/train.component';
+import {LayoutComponent} from './components/layout/layout.component';
+
 
 export const routes: Routes = [
     {
         path:'',
-        loadComponent: () => import('./components/layout/layout.component')
+        component: LayoutComponent
+    },{
+        path:'hotel',
+        component: HotelComponent
+    },{
+        path:'train',
+        component: TrainComponent
     }
 ];
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { HotelComponent } from '../../pages/hotel/hotel.component';
 
 @Component({
   selector: 'app-layout',
@@ -11,6 +12,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export default class LayoutComponent {
+export class LayoutComponent {
 
 }
