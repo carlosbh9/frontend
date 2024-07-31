edc784cc1c6c739c081128137ca7688e94c82515
Carlos <017100872b@uandina.edu.pe>
2024-07-31 12:30:04 -0500

    train version 1

--- src/app/Services/train.service.ts
+++ src/app/Services/train.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { environment } from '../../enviroment/environment';
+import { HttpClient,  } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainService {
+  private baseUrl = `${environment.apiUrl}/train` // Reemplaza con la URL de tu API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Crear un nuevo tren
+  async addTrain(train: any): Promise<any> {
+    try {
+      const response = await firstValueFrom(this.http.post<any>(this.baseUrl, train));
+      return response;
+    } catch (error) {
+      console.error('Error while trying to add Train', error);
+      throw error;
+    }
+  }
+
+  // Obtener todos los trenes
+  async getAllTrains(): Promise<any[]> {
+    try {
+      const response = await firstValueFrom(this.http.get<any[]>(this.baseUrl));
+      return response;
+    } catch (error) {
+      console.error('Error while trying to get all Trains', error);
+      throw error;
+    }
+  }
+
+  // Obtener un tren por ID
+  async getTrainById(id: string): Promise<any> {
+    try {
+      const response = await firstValueFrom(this.http.get<any>(`${this.baseUrl}/${id}`));
+      return response;
+    } catch (error) {
+      console.error('Error while trying to get by Train', error);
+      throw error;
+    }
+  }
+
+  // Actualizar un tren
+  async updateTrain(id: string, train: any): Promise<any> {
+    try {
+      const response = await firstValueFrom(this.http.patch<any>(`${this.baseUrl}/${id}`, train));
+      return response;
+    } catch (error) {
+      console.error('Error while trying to update Train', error);
+      throw error;
+    }
+  }
+
+  // Eliminar un tren
+  async deleteTrain(id: string): Promise<void> {
+    try {
+      await firstValueFrom(this.http.delete(`${this.baseUrl}/${id}`));
+    } catch (error) {
+      console.error('Error while trying to delete train', error);
+      throw error;
+    }
+  }
+
+ 
 }
--- src/app/pages/train/train.component.ts
+++ src/app/pages/train/train.component.ts
@@ -1,12 +1,134 @@
 import { Component } from '@angular/core';
+import { TrainService } from '../../Services/train.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-train',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './train.component.html',
   styleUrl: './train.component.css'
 })
 export class TrainComponent {
+
+  constructor(private trainService: TrainService,private cdr: ChangeDetectorRef) {}
+  trains: any[] = [];
+  filteredTrains: any[] = [];
+  filterText: string = '';
+  showAddModal = false;
+  showEditModal = false;
+
+ newTrain: any = {
+   company: '',
+   services: [{
+     serviceName: '',
+     prices: [{
+       season: 'Regular',
+       adultPrice: 0,
+       childPrice: 0
+     }],
+     observations: ''
+   }]
+ };
+
+ selectedTrain: any = {
+   company: '',
+   services: [{
+     serviceName: '',
+     prices: [{
+       season: 'Regular',
+       adultPrice: 0,
+       childPrice: 0
+     }],
+     observations: ''
+   }]
+ };
+
+
+ ngOnInit() {
+    this.fetchTrains();
+  }
+
+  async fetchTrains() {
+    try {
+      this.trains = await this.trainService.getAllTrains();
+      this.filteredTrains = this.trains;
+    } catch (error) {
+      console.error('Error fetching guides', error);
+    }
+  }
+
+  filterTrains() {
+    this.filteredTrains = this.trains.filter(train =>
+      train.name_train.toLowerCase().includes(this.filterText.toLowerCase()) || train.type_train.toLowerCase().includes(this.filterText.toLowerCase())
+    );
+  }
+
+  async deleteTrain(id: string) {
+    try {
+      await this.trainService.deleteTrain(id);
+      this.fetchTrains();
+    } catch (error) {
+      console.error('Error al eliminar el tren', error);
+    }
+  }
+
+  openEditModal(train: any) {
+    this.selectedTrain = { ...train };
+    this.showEditModal = true;
+    this.cdr.detectChanges();
+  }
+
+  closeEditModal() {
+    this.showEditModal = false;
+  }
+
+  openModal() {
+    this.showAddModal = true;
+  }
+
+  closeModal() {
+    this.showAddModal = false;
+    this.emptyTrain();
+  }
+
+  emptyTrain(): void {
+    this.newTrain = {
+      name_train: '',
+      type_train: 'Regular',
+      price_train: 0,
+      observations: ''
+    };
+  }
+
+  onSubmit() {
+    this.trainService.addTrain(this.newTrain).then(
+      response => {
+        console.log('Tren añadido', response);
+        this.fetchTrains();
+        this.showAddModal = false;
+        this.emptyTrain();
+      },
+      error => {
+        console.error('Error al añadir el tren', error);
+      }
+    );
+  }
+
+  onEditSubmit() {
+    this.trainService.updateTrain(this.selectedTrain._id, this.selectedTrain).then(
+      response => {
+        console.log('Tren actualizado', response);
+        this.fetchTrains();
+        this.showEditModal = false;
+      },
+      error => {
+        console.error('Error al actualizar el tren', error);
+      }
+    );
+  }
 
 }
