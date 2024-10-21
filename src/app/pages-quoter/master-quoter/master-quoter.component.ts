baf967426143357e1f604b6cdbd6b1b7d58254ea
Carlos <017100872b@uandina.edu.pe>
2024-10-21 17:06:00 -0500

    21/10

--- src/app/pages-quoter/master-quoter/master-quoter.component.ts
+++ src/app/pages-quoter/master-quoter/master-quoter.component.ts
@@ -18,132 +18,140 @@ import { OperatorsService } from '../../Services/operators.service';
 })
 export class MasterQuoterComponent implements OnInit{
   
-  hotelService = inject(HotelService)
   entranceService = inject(EntrancesService)
   expeditionsService = inject(ExpeditionsService)
   guidesService = inject(GuidesService)
   restaurantService = inject(RestaurantService)
   operatorsService = inject(OperatorsService)
-  hotel_service: string = ''
-  hotel_selectId: string =''
-  hotelsOption: any[]= []
-
+  
+  typeoptions: string =''
+  selectedDayIndex: any = {type: 'service',dayIndex: 0};
   servicesOptions: any[]=[]
   subservicesOptions: any[]=[]
-
-  hotelServices: any[] = [];
-
   selectCategoria: string =''
-  selectedService: string = ''
-  selectedSubService: string = ''
+  selectedService: any = {}
+  selectedTag: any ={}
+
+  selectedSubService: any = {}
   tags: any[] = []; // Etiquetas seleccionadas
-  ngOnInit(): void {
-    this.loadHotels();
-    //this.loadServices()
-  }
-
-  async loadHotels() {
-    try {
-      this.hotelsOption = await this.hotelService.getAllHotels();
-    //  this.servicesOptions = await this.entranceService.getAllEntrances();
-    } catch (error) {
-      console.error('Error fetching hotels:', error);
-    }
+  options: any[] = []; // Etiquetas seleccionadas
+  index: number = 0
+  temp: number =0
+  masterQuoter = {
+    name: null,
+    days: null,
+    destinations: null,
+    day:[{
+    //  index: 0,
+      services: [] as {
+      type_service: string | null,
+      name_service: string | null,
+      service_id: string | null,
+      service_type: string | null,
+      operator_service_id: string | null,
+      train_service_id: string | null,
+    }[]
+    }],
     
+  };
+  // masterQuoter = {
+  //   name: null,
+  //   days: null,
+  //   destinations: null,
+  //   day:[],
+  //   services: [] as {
+  //     type_service: string | null,
+  //     name_service: string | null,
+  //     service_id: string | null,
+  //     service_type: string | null,
+  //     operator_service_id: string | null,
+  //     train_service_id: string | null,
+  //   }[]
+  // };
+
+  ngOnInit(): void {
+   
   }
 
-  
-  toggleTag(event: Event): void {
-    const tagSelect = event.target as HTMLSelectElement;
-    const selectedId = Number(tagSelect.value); // Obtener el id seleccionado
 
-    const selectedTag = this.hotelServices.find(option => option._id === this.hotel_service);
 
-    if (selectedTag) {
-      const index = this.tags.findIndex(tag => tag.id === selectedTag.id);
-      if (index === -1) {
-        this.tags.push(selectedTag); // Añadir etiqueta
-      } else {
-        this.tags.splice(index, 1); // Eliminar etiqueta
-      }
-
-      console.log('Etiquetas seleccionadas:', this.tags); // Mostrar en consola
-    }
-  }
 
   removeTag(tag: any): void {
-    const index = this.tags.findIndex(t => t.hotels_service_id === tag.hotels_service_id);
+    const index = this.masterQuoter.day[this.index].services.findIndex(t => t.service_id === tag.service_id);
     if (index !== -1) {
-      this.tags.splice(index, 1); // Eliminar el tag
-      console.log('Tags seleccionados:', this.tags); // Mostrar en consola las etiquetas restantes
+      this.masterQuoter.day[this.index].services.splice(index, 1); // Eliminar el tag
+      console.log('Tags seleccionados:', this.masterQuoter.day[this.index].services); // Mostrar en consola las etiquetas restantes
     }
   }
 
-  onHotelChange(event: any): void {
-    const hotel_select = this.hotelsOption.find(hotel => hotel._id === this.hotel_selectId)
-    if(hotel_select){
-      this.hotelServices= hotel_select.services
-     // this.newHotel.name_hotel= hotel_select.name
-    }
-    
-}
 
-onServiceChangeHotel(event: any): void {
-  const serviceSelect = event.target as HTMLSelectElement;
-  const selectedServiceId = serviceSelect.value; // Obtener el id del servicio seleccionado
-  const name = this.hotelServices.find(service => service._id === selectedServiceId);
-
-  // Crear un tag basado en el esquema proporcionado
-  const selectedTag: any = {
-    service_id: this.hotel_selectId,         // ID del hotel seleccionado
-    service_type: 'hotel',                   // Tipo de servicio, en este caso 'hotel'
-    hotels_service_id: selectedServiceId,
-    name_service: name.name_service       // ID del servicio seleccionado
-  };
-
-  // Verificar si el tag ya está en el arreglo de tags
-  const index = this.tags.findIndex(tag => tag.hotels_service_id === selectedTag.hotels_service_id);
-
-  if (index === -1) {
-    this.tags.push(selectedTag); // Añadir nuevo tag si no existe
-  } else {
-    this.tags.splice(index, 1); // Eliminar tag si ya existe
-  }
-
-  console.log('Tags seleccionados:', this.tags); // Mostrar en consola las etiquetas seleccionadas
-  console.log('categoria: ',this.selectCategoria)
-  console.log('options: ',this.servicesOptions)
-  }
 
  async onCategoriaChange(event: any){
-    
-   
     console.log('categoria: ',this.selectCategoria)
     
       switch(this.selectCategoria){
-        case 'Entrances': this.servicesOptions = await this.entranceService.getAllEntrances();  break;
-        case 'Expeditions': this.servicesOptions = await this.expeditionsService.getAllExpeditions(); break;
-        case 'Guides':  this.servicesOptions = await this.guidesService.getAllGuides(); break;
+        case 'entrance': this.servicesOptions = await this.entranceService.getAllEntrances();  break;
+        case 'expeditions': this.servicesOptions = await this.expeditionsService.getAllExpeditions(); break;
+        case 'guides':  this.servicesOptions = await this.guidesService.getAllGuides(); break;
   
-        case 'Restaurants': this.servicesOptions = await this.restaurantService.getAllRestaurants(); break;
+        case 'restaurant': this.servicesOptions = await this.restaurantService.getAllRestaurants(); break;
   
-        case 'Operators': this.servicesOptions = await this.operatorsService.getAllOperators(); 
+        case 'operator': this.servicesOptions = await this.operatorsService.getAllOperators(); 
          break;
   
-        case 'Transporte': 
+        case 'transport': 
   
-        case 'Experiencias':
+        case 'experience':
       }
-    
-      console.log('options: ',this.servicesOptions);
-  }
-  async onServiceChange(event: any){
-    console.log('id service: ',this.selectedService);
-    if(this.selectedService!){this.subservicesOptions = await this.operatorsService.getServicesByOperator(this.selectedService);}
-  }
-prueba(event: any){
   
-  console.log('id sub service: ',this.selectedSubService)
-  console.log('subservicios de operadores y train',this.subservicesOptions)
+  }
+
+async onServiceChange(event: any){
+    
+    if(this.selectCategoria==='operator'){this.subservicesOptions = await this.operatorsService.getServicesByOperator(this.selectedService.service_id);}
+    
+    const index = this.masterQuoter.day[this.selectedDayIndex.dayIndex].services.findIndex(tag => tag.service_id === this.selectedService.service_id);
+   // this.masterQuoter.services[0].options=[]
+    if(this.selectCategoria!='operator'){
+      if (index === -1) {
+          this.masterQuoter.day[this.selectedDayIndex.dayIndex].services.push(this.selectedService); 
+        // Añadir nuevo tag si no existe
+      } else {
+        this.masterQuoter.day[this.selectedDayIndex.dayIndex].services.splice(index, 1); // Eliminar tag si ya existe
+      }
+    }
+   
+   console.log('services ',this.masterQuoter.day)
+}
+prueba(event: any){
+  // this.selectedService.operator_service_id =this.selectedSubService.id 
+  // this.selectedService.name_service=this.selectedSubService.name
+  // Verificar si el tag ya está en el arreglo de tags
+ // this.selectedSubService.type_service= this.typeoptions
+  this.selectedSubService.type_service= this.selectedDayIndex.type
+  this.selectedSubService.service_id = this.selectedService.service_id
+  this.selectedSubService.service_type= this.selectCategoria
+  const index = this.masterQuoter.day[this.selectedDayIndex.dayIndex].services.findIndex(tag => tag.operator_service_id === this.selectedSubService.operator_service_id
+  );
+
+  console.log('index',index)
+  if (index === -1) {
+      this.masterQuoter.day[this.selectedDayIndex.dayIndex].services.push(this.selectedSubService); 
+  } else {
+    this.masterQuoter.day[this.selectedDayIndex.dayIndex].services.splice(index, 1); // Eliminar tag si ya existe
+  }
+  //console.log('services',this.selectedSubService.id)
+  console.log('services 2',this.masterQuoter.day)
+ 
+}
+
+addDays(){
+  this.index++
+  this.masterQuoter.day.push({
+ //   index: this.index,
+    services: [] 
+  })
+
+  console.log('typeoptions',this.selectedDayIndex)
 }
 }
