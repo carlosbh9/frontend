5e5df1acb13d3dca8cfb2b3c2e8500b7f6cd24fe
Carlos <017100872b@uandina.edu.pe>
2024-11-25 17:01:15 -0500

    25/11

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,7 @@
-export interface HotelService {
-  name_hotel: string;
-  type_hotel: string;
-  price: number[];
-  accomodatios_category: string;
-  notes: string;
-}
 
-export interface Hotel {
-  day: string;
-  date: string;
-  city: string;
-  hotelservices: HotelService[];
-}
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { EntrancesService } from '../Services/entrances.service';
 
 @Component({
   selector: 'app-login',
@@ -24,45 +10,6 @@ import { EntrancesService } from '../Services/entrances.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit{
-  service = inject(EntrancesService)
-  insertPosition: number =0
-  flights: any[] = [];
+export class LoginComponent{
 
-  moveFlightUp(index: number) {
-    if (index > 0) {
-      // Intercambiar la fila actual con la anterior
-      const temp = this.flights[index];
-      this.flights[index] = this.flights[index - 1];
-      this.flights[index - 1] = temp;
-    }
-  }
-  
-  moveFlightDown(index: number) {
-    if (index < this.flights.length - 1) {
-      // Intercambiar la fila actual con la siguiente
-      const temp = this.flights[index];
-      this.flights[index] = this.flights[index + 1];
-      this.flights[index + 1] = temp;
-    }
-  }
-
-  onEdit(i:number){
-
-  }
-  onDelete(i:number){}
-  ngOnInit(): void {
-    this.fetchEntrances();
-  }
-
-  async fetchEntrances() {
-    try {
-      this.flights= await this.service.getAllEntrances();
-      
-     // this.entrances = data;
-      console.log(this.flights)
-    } catch (error) {
-      console.error('Error fetching entrances', error);
-    }
-  }
 }
